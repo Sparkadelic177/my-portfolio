535dd2aa8e9dcac03bf453df6f85941e35474009
Sparkadelic177 <sparkyevangelista@gmail.com>
2020-02-11 13:06:17 -0500

    Updates

---
+++ precache-manifest.827207c3c20b5519fe2918cfb36a27ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658bc336dcf205592550441ba3932253",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "63b0106a3219d640dcca",
+    "url": "/my-portfolio/static/css/main.40add8df.chunk.css"
+  },
+  {
+    "revision": "44874d2f5ebb207a0807",
+    "url": "/my-portfolio/static/js/2.8efd3690.chunk.js"
+  },
+  {
+    "revision": "d9bee4e26266dab8796c9ed1573a3e49",
+    "url": "/my-portfolio/static/js/2.8efd3690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b0106a3219d640dcca",
+    "url": "/my-portfolio/static/js/main.0baf574d.chunk.js"
+  },
+  {
+    "revision": "1967912a364bfc562aab",
+    "url": "/my-portfolio/static/js/runtime-main.28380617.js"
+  },
+  {
+    "revision": "8a116e0acf74c73dcee645dae9687ec8",
+    "url": "/my-portfolio/static/media/github.8a116e0a.png"
+  },
+  {
+    "revision": "ab46037f2bb51cacbd0e7bea5a8cf3a6",
+    "url": "/my-portfolio/static/media/linkedin.ab46037f.svg"
+  },
+  {
+    "revision": "57c7706f2eef31fbcd8598d48f59c52e",
+    "url": "/my-portfolio/static/media/medium.57c7706f.png"
+  },
+  {
+    "revision": "bd75899cc6260fc91f3cc2f4ec47f9ac",
+    "url": "/my-portfolio/static/media/sparky.bd75899c.jpg"
+  },
+  {
+    "revision": "a1994736ad75ab94e8733fe148b6e44e",
+    "url": "/my-portfolio/static/media/telegram.a1994736.svg"
+  },
+  {
+    "revision": "4f41a9846006dbe195ddcf426c32c502",
+    "url": "/my-portfolio/static/media/twitter.4f41a984.svg"
+  }
+]);
