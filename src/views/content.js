ae471fcabc6e515c3244e439d205246c7e1bfe9d
Sparkadelic177 <sparkyevangelista@gmail.com>
2020-02-11 06:33:54 -0500

    new build

--- src/views/content.js
+++ src/views/content.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../styles/content.css";
 
+// todo: https://developers.google.com/youtube/v3/docs/search/list?apix_params=%7B%22part%22%3A%22snippet%22%2C%22forMine%22%3Atrue%2C%22type%22%3A%22video%22%7D
+// api Key, forMine = true, type = video, part = snippet
+
 //this page is going to hold all of my medium blogs and youtube videos
 export default class Content extends Component {
   constructor(props) {
