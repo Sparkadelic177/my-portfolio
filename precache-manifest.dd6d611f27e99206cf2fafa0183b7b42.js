239fff5009d0e15c089b79a280c1cc968694e8e7
Sparkadelic177 <sparkyevangelista@gmail.com>
2020-02-18 18:28:52 -0500

    Updates

---
+++ precache-manifest.dd6d611f27e99206cf2fafa0183b7b42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7e9d4411919513bebd301ab93b403d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eebfdac1878b50c0254",
+    "url": "/static/css/main.a860369c.chunk.css"
+  },
+  {
+    "revision": "52f756ae721ebd70969a",
+    "url": "/static/js/2.46e07af4.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.46e07af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eebfdac1878b50c0254",
+    "url": "/static/js/main.af82ac66.chunk.js"
+  },
+  {
+    "revision": "c4f0eeaac54a21b9f214",
+    "url": "/static/js/runtime-main.0e7bc59a.js"
+  },
+  {
+    "revision": "8a116e0acf74c73dcee645dae9687ec8",
+    "url": "/static/media/github.8a116e0a.png"
+  },
+  {
+    "revision": "ab46037f2bb51cacbd0e7bea5a8cf3a6",
+    "url": "/static/media/linkedin.ab46037f.svg"
+  },
+  {
+    "revision": "57c7706f2eef31fbcd8598d48f59c52e",
+    "url": "/static/media/medium.57c7706f.png"
+  },
+  {
+    "revision": "bd75899cc6260fc91f3cc2f4ec47f9ac",
+    "url": "/static/media/sparky.bd75899c.jpg"
+  },
+  {
+    "revision": "a1994736ad75ab94e8733fe148b6e44e",
+    "url": "/static/media/telegram.a1994736.svg"
+  },
+  {
+    "revision": "4f41a9846006dbe195ddcf426c32c502",
+    "url": "/static/media/twitter.4f41a984.svg"
+  }
+]);
